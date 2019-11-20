2ab116afa07e85213e401f7fcf6fb0273fad26a6
Gavin Butler <gavinrossbutler@gmail.com>
2019-11-19 21:20:48 -0500

    search functionality working

--- DiaryApp/Controller/DiaryListController.swift
+++ DiaryApp/Controller/DiaryListController.swift
@@ -9,14 +9,17 @@
 import UIKit
 import CoreData
 
+//Displays main tableview showing list of items
 class DiaryListController: UITableViewController {
 
+    //Set the context for CRUD operations – need to ensure this specific context is used by any other controllers wishing to retrieve or modify data
     let managedObjectContext = CoreDataStack().managedObjectContext
     
     lazy var dataSource: DataSource = {
         return DataSource(tableView: self.tableView, context: self.managedObjectContext)
     }()
     
+    //Header view to display today’s date
     lazy var headerView: UITableViewCell = {
         let cell = Bundle.main.loadNibNamed("HeaderCell", owner: self, options: nil)?.first as! HeaderCell
         cell.dateLabel.text = Date().formattedMmmDDYYYY()
@@ -30,33 +33,50 @@ class DiaryListController: UITableViewController {
         
         super.viewDidLoad()
         
+        //Set tableView datasource and self as search delegate
         tableView.dataSource = dataSource
         
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.backgroundColor = .white
+        searchController.searchBar.placeholder = "Enter search term"
+        
+        definesPresentationContext = true
+        navigationItem.searchController = searchController
+//
+        searchController.searchBar.delegate = self
+        
+        //ViewController title and navigation bar item configurations
         configureUI()
         
     }
     
     //MARK: - Table View Delegate Methods:
     
+    //Allow swipe to delete on the tableView rows
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        guard let detailViewController = segue.destination as? DetailViewController else { return }
+        //Should only be segueing to the detail VC here.
+        guard segue.identifier == "showDetail", let detailViewController = segue.destination as? DetailViewController else { return }
         
+        //Set context and item using selected tableView row (dependency injection)
         detailViewController.context = self.managedObjectContext
-        
-        if segue.identifier == "showDetail" {
-            if let indexPath = tableView.indexPathForSelectedRow {
-                let item = dataSource.object(at: indexPath)
-                detailViewController.item = item
-
-            }
+        if let indexPath = tableView.indexPathForSelectedRow {
+            let item = dataSource.object(at: indexPath)
+            detailViewController.item = item
         }
+        
+        //searchController.isActive = false
+        //searchController.searchBar.isHidden = true
+        
+        //tableView.tableHeaderView = nil
     }
     
+    //TableView delegate methods to establish header view
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         return headerView
     }
@@ -72,35 +92,46 @@ extension DiaryListController {
     func configureUI() {
         self.title = Date().formattedMmmDDYYYY()
         
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(DiaryListController.refreshSearchResults))
-        
-        tableView.tableHeaderView = searchController.searchBar
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(DiaryListController.showSearchBar))
+        self.navigationItem.leftBarButtonItem?.isEnabled = true
+    }
+    
+    @objc func showSearchBar() {
+        //Show the search bar when user selects the search symbol (navbar left item).
+        //tableView.tableHeaderView = searchController.searchBar
+        navigationItem.searchController = searchController
         searchController.dimsBackgroundDuringPresentation = false
         searchController.searchResultsUpdater = self
         definesPresentationContext = true
     }
     
-    @objc func refreshSearchResults() {
-        //self.dismiss(animated: true, completion: nil)
-        
-    }
-    
 }
 
-extension DiaryListController: UISearchResultsUpdating {
+//MARK: - SearchBarController and SearchBar Delegate Methods:
+extension DiaryListController: UISearchResultsUpdating, UISearchBarDelegate {
     
+    //Delegate method triggered for each entry in the searchbar.  Used as filter passed to the fetchedResultsController via setting it’s predicate.
     func updateSearchResults(for searchController: UISearchController) {
         print("searchbar text is: \(searchController.searchBar.text!)")
         
+        //Clear any prior filter (filter should stay removed if user has backspaced to have no text in the searchbar)
         self.dataSource.fetchedResultsController.fetchRequest.predicate = nil
         
+        //If you have text, use it as the filter – create & set the predicate
         if !searchController.searchBar.text!.isEmpty {
             self.dataSource.fetchedResultsController.fetchRequest.predicate = NSPredicate(format: "text CONTAINS[cd] %@", searchController.searchBar.text!)
         }
         
+        //Fetch data with the new filter, reload the tableView to update the UI
         self.dataSource.fetchedResultsController.tryFetch()
         
         tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print("Searchbar cancel selected")
+        tableView.tableHeaderView = nil
+        self.navigationItem.leftBarButtonItem?.isEnabled = true
     }
     
 }
