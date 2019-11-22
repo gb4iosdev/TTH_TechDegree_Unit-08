d24bf52a59860754d066f2e2a7cf66903ba0c542
Gavin Butler <gavinrossbutler@gmail.com>
2019-11-21 19:52:44 -0500

    Final comments

--- DiaryApp/PhotoManager/PhotoPickerController.swift
+++ DiaryApp/PhotoManager/PhotoPickerController.swift
@@ -1,8 +1,7 @@
 //
 //  ViewController.swift
-//  ImagePickerExample
 //
-//  Created by Dennis Parussini on 29.10.19.
+//  Attribution:  ImagePickerExample,  Created by Dennis Parussini on 29.10.19.
 //  Copyright © 2019 Dennis Parussini. All rights reserved.
 //
 
@@ -12,7 +11,7 @@ final class PhotoPickerController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    //Set up the image picker. In this simple example we're either showing the camera or the photo library, but in a real world app you definitely want the user to be able to choose. Also, the camera doesn't work in simulators.
+    //Set up the image picker - either showing the camera or the photo library
     var imagePicker: UIImagePickerController = {
         let picker = UIImagePickerController()
         
@@ -51,10 +50,10 @@ extension PhotoPickerController: UIImagePickerControllerDelegate {
     //This gets called when you take a picture with the camera or choose one from the photo library.
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
-        //Parse the image and typecast it to a UIImage to use somewhere else in your view controller
+        //Parse the image and typecast it to a UIImage
         guard let image = info[.originalImage] as? UIImage else { return }
         
-        //Always dismiss the image picker when you're done with it. In this case I assign the image to the image view, but you can also just assign it to a property, save it in Core Data or whatever you need to do with it. This also means you can first assign the image to something and then dismiss the image picker.
+        //Dismiss the image picker when you're done with it.
         picker.dismiss(animated: true) {
             self.imageView.image = image
         }
